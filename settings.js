8af819e9fb4b53d848f4a3148545730abf966904
uchihaobito78654 <163227557+uchihaobito78654@users.noreply.github.com>
2024-03-21 20:42:41 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -50,7 +50,7 @@ global.location = process.env.GL_INFO || "Pakistan, Lahore, Sabzazar" //ur locat
 
 //new
 //________________________________________________________________________________________________________
-global.botname = process.env.BOT_NAME || 'XLICON-V3-MD' //ur bot name
+global.botname = process.env.BOT_NAME || 'RAJA-OFFICIAL' //ur bot name
 //________________________________________________________________________________________________________
 global.ownernumber = process.env.SUDO || '923184070915' //ur owner number
 //________________________________________________________________________________________________________
